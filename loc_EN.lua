bcbf20155c23f093de6d1a7d543f33ece7aa4a94
alexjthomson1882 <alexthomson666@gmail.com>
2021-07-09 11:23:34 +0100

    Initial commit

---
+++ loc_EN.lua
@@ -0,0 +1,70 @@
+local text_original = LocalizationManager.text
+function LocalizationManager:text(string_id, ...)
+	return string_id == "all_2" and
+			"Helmet Popping"
+		or string_id == "all_2_desc" and
+			"Increase your headshot damage by ##25%##."
+		or string_id == "all_4" and
+			"Blending In"
+		or string_id == "all_4_desc" and
+			"You gain ##+1## increased concealment.\n\nWhen wearing armor, your movement speed is ##15%## less affected.\n\nYou gain ##45%## more experience when you complete days and jobs." 
+		or string_id == "all_6" and
+			"Walk-in Closet"
+		or string_id == "all_6_desc" and
+			"Unlocks an armor bag equipment for you to use.\nThe armor bag can be used to change your armor during a heist.\n\nIncreases your ammo pickup to ##135%## of the normal rate."
+		or string_id == "all_8" and
+			"Fast and Furious"
+		or string_id == "all_8_desc" and
+			"You do ##5%## more damage. Does not apply to melee damage, throwables, grenade launchers or rocket launchers.\n\nIncreases your doctor bag interaction speed by ##20%##."
+		-- custom alcoholic --
+		or string_id == "custom_alcoholic'name" and
+			"Alcoholic"
+		or string_id == "custom_alcoholic'desc" and
+			"A much more overpowered version of the stoic perk deck."
+		or string_id == "custom_alcoholic'perk1n" and
+			"Alcoholic"
+		or string_id == "custom_alcoholic'perk1d" and
+			"You're wearing an armor of alcohol, assisted by the stoics hip flask. Your health is greatly increased and you can regenerate health with the stoic hip flask."
+		or string_id == "custom_alcoholic'perk3n" and
+			"Stronger Spirits"
+		or string_id == "custom_alcoholic'perk3d" and
+			"The alcohol you drink is stronger, you feel your wounds heal over time."
+		or string_id == "custom_alcoholic'perk5n" and
+			"Pub Skills"
+		or string_id == "custom_alcoholic'perk5d" and
+			"Many drunken pub brawls has taught you improved combat skills up close and at range."
+		or string_id == "custom_alcoholic'perk7n" and
+			"On The Run"
+		or string_id == "custom_alcoholic'perk7d" and
+			"After your pub brawls you have gotten very good at avoiding the law. You have improved stamina, speed, and dodge. Police find it harder to notice you because they're stunned by the fumes of alcohol coming off of you."
+		or string_id == "custom_alcoholic'perk9n" and
+			"Seasoned Brawler"
+		or string_id == "custom_alcoholic'perk9d" and
+			"Your skills as an alcoholic are mature, you gain minor improvements to all stats and gain additional exp from heists."
+		-- custom stoic mk ii --
+		or string_id == "custom_stoic'name" and
+			"Stoic MK II"
+		or string_id == "custom_stoic'desc" and
+			"A slightly more over powered stoic perk deck."
+		or string_id == "custom_stoic'perk1n" and
+			"Stoic Skills"
+		or string_id == "custom_stoic'perk1d" and
+			"Basic stoic skills.."
+		or string_id == "custom_stoic'perk3n" and
+			"Health Buffs"
+		or string_id == "custom_stoic'perk3d" and
+			"You get slight health buffs."
+		or string_id == "custom_stoic'perk5n" and
+			"Combat Skills"
+		or string_id == "custom_stoic'perk5d" and
+			"You get skills for combat."
+		or string_id == "custom_stoic'perk7n" and
+			"Speedy Cunt"
+		or string_id == "custom_stoic'perk7d" and
+			"You have stamina, speed, and dodge buffs. You're also a bit sneakier while crouched."
+		or string_id == "custom_stoic'perk9n" and
+			"Seasoned Brawler"
+		or string_id == "custom_stoic'perk9d" and
+			"You gain additional EXP and other small buffs."
+		or text_original(self, string_id, ...)
+	end
---
+++ skilltree.lua
@@ -0,0 +1,267 @@
+local data = SkillTreeTweakData.init
+function SkillTreeTweakData:init(tweak_data)
+	data(self, tweak_data)
+	-- psX = skill X
+	-- pcX = skill cost X
+	local ps2 = {
+		upgrades = {"weapon_passive_headshot_damage_multiplier"},
+		cost = 300,
+		icon_xy = {1, 0},
+		name_id = "all_2",
+		desc_id = "all_2_desc"
+	}
+	local ps4 = {
+		upgrades = {
+			"passive_player_xp_multiplier",
+			"player_passive_suspicion_bonus",
+			"player_passive_armor_movement_penalty_multiplier"
+		},
+		cost = 600,
+		icon_xy = {3, 0},
+		name_id = "all_4",
+		desc_id = "all_4_desc"
+	}
+	local ps6 = {
+		upgrades = {
+			"armor_kit",
+			"player_pick_up_ammo_multiplier"
+		},
+		cost = 1600,
+		icon_xy = {5, 0},
+		name_id = "all_6",
+		desc_id = "all_6_desc"
+	}
+	local ps8 = {
+		upgrades = {
+			"weapon_passive_damage_multiplier",
+			"passive_doctor_bag_interaction_speed_multiplier"
+		},
+		cost = 3200,
+		icon_xy = {7, 0},
+		name_id = "all_8",
+		desc_id = "all_8_desc"
+	}
+	local pc1 = 200
+	local pc3 = 300
+	local pc5 = 400
+	local pc7 = 600
+	local pc9 = 1000
+	local pdcb = "player_passive_loot_drop_multiplier"
+	-- ALCOHOLIC:
+	table.insert(self.specializations, {
+		name_id = "custom_alcoholic'name",
+		desc_id = "custom_alcoholic'desc",
+		{ -- skill 1 - STOIC & HEALTH
+			upgrades = {
+				"player_damage_control_passive",
+				"temporary_damage_control",
+				"damage_control",
+				"player_damage_control_cooldown_drain_1",
+				"player_tier_armor_multiplier_1",
+				"player_tier_armor_multiplier_2",
+				"player_tier_armor_multiplier_3",
+				"player_tier_armor_multiplier_4",
+				"player_tier_armor_multiplier_5",
+				"player_tier_armor_multiplier_6"
+			},
+			cost = pc1,
+			icon_xy = {0, 0},
+			name_id = "custom_alcoholic'perk1n",
+			desc_id = "custom_alcoholic'perk1d"
+		},
+		ps2, -- skill 2 (all)
+		{ -- skill 3 - ADDITIONAL HEALTH AND REGEN
+			upgrades = {
+				"player_armor_to_health_conversion",
+				"player_damage_control_auto_shrug",
+				"player_damage_control_cooldown_drain_2",
+				"player_passive_health_multiplier_1", -- muscle
+				"player_passive_health_multiplier_2", -- muscle
+				"player_passive_health_multiplier_3", -- muscle
+				"player_passive_health_multiplier_4", -- muscle
+				"player_level_2_armor_multiplier_1",
+				"player_level_2_armor_multiplier_2",
+				"player_level_2_armor_multiplier_3",
+				"player_level_3_armor_multiplier_1",
+				"player_level_3_armor_multiplier_2",
+				"player_level_3_armor_multiplier_3",
+				"player_level_4_armor_multiplier_1",
+				"player_level_4_armor_multiplier_2",
+				"player_level_4_armor_multiplier_3",
+				"player_passive_health_regen", -- muscle
+				"player_damage_control_healing",
+				"player_kill_change_regenerate_speed", -- muscle
+				"player_damage_dampener_close_contact_1",
+				"player_damage_dampener_close_contact_2",
+				"player_damage_dampener_close_contact_3",
+				"player_damage_dampener_outnumbered_strong"
+			},
+			cost = pc3,
+			icon_xy = {1, 1},
+			name_id = "custom_alcoholic'perk3n",
+			desc_id = "custom_alcoholic'perk3d"
+		},
+		ps4, -- skill 4 (all)
+		{ -- skill 5 - ADDITIONAL COMBAT SKILLS
+			upgrades = {
+				"player_dmg_interval_health_ratio_multiplier", -- yakuza
+				"player_damage_to_hot_1", -- grinder
+				"player_damage_to_hot_2", -- grinder
+				"player_damage_to_hot_3", -- grinder
+				"player_damage_to_hot_4", -- grinder
+				"player_panic_when_kill", -- muscle
+				"player_killshot_close_panic_chance",
+				"weapon_passive_armor_piercing_chance",
+				"player_melee_kill_life_leech",
+				"player_melee_life_leech",
+				"player_armor_piercing_chance_1",
+				"player_armor_piercing_chance_2",
+				"player_passive_intimidate_range_mul"
+			},
+			cost = pc5,
+			icon_xy = {1, 1},
+			name_id = "custom_alcoholic'perk5n",
+			desc_id = "custom_alcoholic'perk5d"
+		},
+		ps6, -- skill 6 (all)
+		{ -- skill 7 - CAMOFLAGE, STAMINA, DODGE, AND SPEED SKILLS
+			upgrades = {
+				"player_pick_lock_speed_multiplier",
+				"player_tier_dodge_chance_1",
+				"player_tier_dodge_chance_2",
+				"player_tier_dodge_chance_3",
+				"weapon_passive_swap_speed_multiplier_1",
+				"player_movement_speed_damage_health_ratio_multiplier",
+				"player_movement_speed_damage_health_ratio_threshold_multiplier",
+				"player_stand_still_crouch_camouflage_bonus_1",
+				"player_stand_still_crouch_camouflage_bonus_2",
+				"player_stand_still_crouch_camouflage_bonus_3",
+				"team_passive_stamina_multiplier_1",
+				"player_corpse_dispose_speed_multiplier",
+				"player_camouflage_multiplier",
+				"player_level_2_dodge_addend_1",
+				"player_level_2_dodge_addend_2",
+				"player_level_2_dodge_addend_3",
+				"player_level_3_dodge_addend_1",
+				"player_level_3_dodge_addend_2",
+				"player_level_3_dodge_addend_3",
+				"player_level_4_dodge_addend_1",
+				"player_level_4_dodge_addend_2",
+				"player_level_4_dodge_addend_3"
+			},
+			cost = pc7,
+			icon_xy = {1, 1},
+			name_id = "custom_alcoholic'perk7n",
+			desc_id = "custom_alcoholic'perk7d"
+		},
+		ps8, -- skill 8 (all)
+		{ -- skill 9 - MISC
+			upgrades = {
+				"passive_player_xp_multiplier",
+				"player_xp_multiplier",
+				"team_xp_multiplier"
+			},
+			cost = pc9,
+			icon_xy = {1, 1},
+			name_id = "custom_alcoholic'perk9n",
+			desc_id = "custom_alcoholic'perk9d"
+		}
+	})
+	-- STOIC MK II
+	table.insert(self.specializations, {
+		name_id = "custom_stoic'name",
+		desc_id = "custom_stoic'desc",
+		{ -- skill 1 - STOIC & HEALTH
+			upgrades = {
+				"player_damage_control_passive",
+				"temporary_damage_control",
+				"damage_control",
+				"player_damage_control_cooldown_drain_1",
+				"player_tier_armor_multiplier_1",
+				"player_tier_armor_multiplier_2"
+			},
+			cost = pc1,
+			icon_xy = {0, 0},
+			name_id = "custom_stoic'perk1n",
+			desc_id = "custom_stoic'perk1d"
+		},
+		ps2, -- skill 2 (all)
+		{ -- skill 3 - ADDITIONAL HEALTH AND REGEN
+			upgrades = {
+				"player_armor_to_health_conversion",
+				"player_damage_control_auto_shrug",
+				"player_damage_control_cooldown_drain_2",
+				"player_passive_health_multiplier_1", -- muscle
+				"player_damage_control_healing",
+				"player_kill_change_regenerate_speed", -- muscle
+				"player_damage_dampener_close_contact_1",
+				"player_damage_dampener_outnumbered_strong"
+			},
+			cost = pc3,
+			icon_xy = {1, 1},
+			name_id = "custom_stoic'perk3n",
+			desc_id = "custom_stoic'perk3d"
+		},
+		ps4, -- skill 4 (all)
+		{ -- skill 5 - ADDITIONAL COMBAT SKILLS
+			upgrades = {
+				"player_dmg_interval_health_ratio_multiplier", -- yakuza
+				"player_panic_when_kill", -- muscle
+				"player_killshot_close_panic_chance",
+				"weapon_passive_armor_piercing_chance",
+				"player_melee_kill_life_leech",
+				"player_melee_life_leech",
+				"player_armor_piercing_chance_1",
+				"player_armor_piercing_chance_2",
+				"player_passive_intimidate_range_mul"
+			},
+			cost = pc5,
+			icon_xy = {1, 1},
+			name_id = "custom_stoic'perk5n",
+			desc_id = "custom_stoic'perk5d"
+		},
+		ps6, -- skill 6 (all)
+		{ -- skill 7 - CAMOFLAGE, STAMINA, DODGE, AND SPEED SKILLS
+			upgrades = {
+				"player_pick_lock_speed_multiplier",
+				"player_tier_dodge_chance_1",
+				"player_tier_dodge_chance_2",
+				"player_tier_dodge_chance_3",
+				"weapon_passive_swap_speed_multiplier_1",
+				"player_movement_speed_damage_health_ratio_multiplier",
+				"player_movement_speed_damage_health_ratio_threshold_multiplier",
+				"player_stand_still_crouch_camouflage_bonus_1",
+				"player_stand_still_crouch_camouflage_bonus_2",
+				"player_stand_still_crouch_camouflage_bonus_3",
+				"team_passive_stamina_multiplier_1",
+				"player_corpse_dispose_speed_multiplier",
+				"player_camouflage_multiplier",
+				"player_level_2_dodge_addend_1",
+				"player_level_2_dodge_addend_2",
+				"player_level_2_dodge_addend_3",
+				"player_level_3_dodge_addend_1",
+				"player_level_3_dodge_addend_2",
+				"player_level_3_dodge_addend_3",
+				"player_level_4_dodge_addend_1",
+				"player_level_4_dodge_addend_2",
+				"player_level_4_dodge_addend_3"
+			},
+			cost = pc7,
+			icon_xy = {1, 1},
+			name_id = "custom_stoic'perk7n",
+			desc_id = "custom_stoic'perk7d"
+		},
+		ps8, -- skill 8 (all)
+		{ -- skill 9 - MISC
+			upgrades = {
+				"passive_player_xp_multiplier",
+				"player_xp_multiplier",
+				"team_xp_multiplier"
+			},
+			cost = pc9,
+			icon_xy = {1, 1},
+			name_id = "custom_stoic'perk9n",
+			desc_id = "custom_stoic'perk9d"
+		}
+	})
+end
